94e4a42387712574fb06c4e4577de51cf506cd66
Контуш Вадим <vadimkontush@gmail.com>
2019-03-21 19:56:49 +0300

    03_Third Task Max Value

--- IT_Practice/Program.cs
+++ IT_Practice/Program.cs
@@ -11,7 +11,34 @@ namespace IT_Practice
         static void Main(string[] args)
         {
             //EmptyArr();
-            ArrThreeElements();
+            //ArrThreeElements();
+            FindMaxElementInArray();
+            
+            
+            void FindMaxElementInArray()
+            {
+                Random rnd = new Random();
+                int[] arrNumbers = new int[13];
+
+                for (int i = 0; i < arrNumbers.Length; i++)
+                {
+                    int rndNumber = rnd.Next(0, 100);
+                    arrNumbers[i] = rndNumber;
+                }
+
+                int maxValue = 0;
+
+                for (int i = 0; i < arrNumbers.Length; i++)
+                {
+                    
+
+                    if (maxValue < arrNumbers[i])
+                    {
+                        maxValue = arrNumbers[i];
+                    }
+                }
+                Console.WriteLine(maxValue);
+            }
 
             void ArrThreeElements()
             {
