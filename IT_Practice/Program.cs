dc4204bdb12df7dff298d09c75bfdb7633356930
Контуш Вадим <vadimkontush@gmail.com>
2019-04-16 20:25:25 +0300

    Dont work (

--- IT_Practice/Program.cs
+++ IT_Practice/Program.cs
@@ -10,6 +10,34 @@ namespace IT_Practice
     {
         static void Main(string[] args)
         {
+            UniqueItem uniqueItem = new UniqueItem();
+            UniqueItem uniqueItem2 = new UniqueItem();
+            UniqueItem uniqueItem3 = new UniqueItem();
+            Console.WriteLine(uniqueItem.Id);
+            Console.WriteLine(uniqueItem2.Id);
+            Console.WriteLine(uniqueItem3.Id);
+
+            Console.ReadLine();
+        }
+    }
+
+    class UniqueItem
+    {
+        public int Id { get; set; }
+        public int IdTemp { get; set; }
+
+        public UniqueItem()
+        {
+            Id = IdTemp;
+            Id++;
+            IdTemp = Id;
+        }
+
+        public UniqueItem(int id)
+        {
+            Id = id;
+            id++;
+            IdTemp = id;
         }
     }
 }
