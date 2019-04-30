f0ada7fd32c2d7dc357bcf0474ec81014a528280
Контуш Вадим <vadimkontush@gmail.com>
2019-04-30 21:40:12 +0300

    Copy Create Folder

--- IT_Practice/Program.cs
+++ IT_Practice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace IT_Practice
 {
@@ -10,6 +11,28 @@ namespace IT_Practice
     {
         static void Main(string[] args)
         {
+            Text.WindowsCopy();
+
+            FileInfo fileinfo = new FileInfo(@"D:\students\Kontush\Projects\kadlk\IT_Practice\Parser_testing\bin\Debug\txt.txt");
+
+            //var name = fileinfo.Name;
+            //var length = fileinfo.Length;
+            //var canWrite = fileinfo.CreationTime;
+            //var creationTimeUtc = fileinfo.CreationTimeUtc;
+
+            FileStream fs = fileinfo.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            byte[] guys = Encoding.Default.GetBytes("Hello Guys!");
+            fs.Write(guys, 0, guys.Length);
+            fs.Close();
+
+            fs = fileinfo.Open(FileMode.Open, FileAccess.Read);
+            byte[] text = new byte[fs.Length];
+            fs.Read(text, 0, (int)fs.Length);
+            fs.Close();
+            Console.WriteLine(Encoding.Default.GetChars(text));
+
+            Console.Read();
         }
     }
 }
---
+++ IT_Practice/Text.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IT_Practice
+{
+    class Text
+    {
+        public static void GetDirectoryInfo()
+        {
+            DirectoryInfo fileinfo = new DirectoryInfo(@"C:\Program Files");
+
+            Console.WriteLine($"Creation date: {fileinfo.CreationTimeUtc}");
+        }
+
+        public static void GetDirectoryFiles()
+        {
+            DirectoryInfo dirinfo = new DirectoryInfo(@"C:\Windows");
+
+            FileInfo[] fileInfos = dirinfo.GetFiles();
+
+            foreach (var item in fileInfos)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        public static void WindowsCopy()
+        {
+            const string NewDirectoryName = @"C:\WindowsCopy";
+            Directory.CreateDirectory(NewDirectoryName);
+            DirectoryInfo dirinfo = new DirectoryInfo(@"C:\Windows");
+            var firstFile = dirinfo.GetFiles().FirstOrDefault();
+
+            firstFile.CopyTo(NewDirectoryName + "\\" + firstFile);
+        }
+    }
+}
